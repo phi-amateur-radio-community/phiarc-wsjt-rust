6e8b8c51a4ac5521493b1490f91f29f82c9feb62
St Rangeset <2432647223@qq.com>
2026-02-11 19:06:40 +0800

    add(encode): added implementation of encode 91 bits

--- src/api/ftx/common.rs
+++ src/api/ftx/common.rs
@@ -6,11 +6,18 @@
 // FTx common API.
 
 use super::msg;
-use bitvec::prelude::{BitVec, Msb0};
+use crate::util::ftx::crc;
+use bitvec::{
+    bitvec,
+    prelude::{BitVec, Msb0},
+};
 
 pub fn encode77(content: msg::Msg) -> Option<BitVec<u8, Msb0>> {
     content.encode()
 }
 
-//TODO encode91
-//TODO encode174
+pub fn encode91(bits: &mut BitVec<u8, Msb0>) {
+    let g: BitVec<u8, Msb0> = bitvec![u8, Msb0; 1, 0, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1];
+    let crc_bits = crc::get_crc(bits, &g);
+    bits.extend(crc_bits);
+}
